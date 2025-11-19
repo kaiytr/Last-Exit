b2cd0106490d943e57aa00693bd57d17644a20cd
kaiytr <iseungchan838@gmail.com>
2025-11-19 11:50:43 +0900

    시야 기능 구현

--- Assets/Scripts/LightManager.cs
+++ Assets/Scripts/LightManager.cs
@@ -1,16 +1,68 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering.Universal;
 
-public class LightManager : MonoBehaviour
+public class FullVisionSkill : MonoBehaviour
 {
-   public UnityEngine.Rendering.Universal.Light2D light2D;
-    void Start()
+    public Light2D visionLight;
+
+    [Header("시야 설정")]
+    public float originalRadius = 5f;
+    public float fullRadius = 50f;
+
+    [Header("스킬 효과 설정")]
+    public float fadeDuration = 0.5f;
+    public float fullVisionHoldTime = 1f;
+
+    private bool isSkillActive = false;
+
+    private void Start()
     {
-        light2D.pointLightOuterRadius = 3.0f;
+        if (visionLight != null)
+        {
+            visionLight.pointLightOuterRadius = originalRadius;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && !isSkillActive)
+        {
+            ActivateSkill();
+        }
+    }
+
+    public void ActivateSkill()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FullVisionCoroutine());
+    }
+
+    IEnumerator FullVisionCoroutine()
+    {
+        isSkillActive = true;
+
+        yield return StartCoroutine(ChangeRadius(visionLight.pointLightOuterRadius, fullRadius, fadeDuration));
+
+        yield return new WaitForSeconds(fullVisionHoldTime);
+
+        yield return StartCoroutine(ChangeRadius(visionLight.pointLightOuterRadius, originalRadius, fadeDuration));
+
+        isSkillActive = false;
+    }
+
+    IEnumerator ChangeRadius(float startRadius, float endRadius, float duration)
+    {
+        float time = 0;
+
+        while (time < duration)
+        {
+            visionLight.pointLightOuterRadius = Mathf.Lerp(startRadius, endRadius, time / duration);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        visionLight.pointLightOuterRadius = endRadius;
     }
 }
